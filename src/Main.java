375257297f8c516c82946a45ff82637ac5700dea
mcguiron <mcguiron@mail.uc.edu>
2024-09-19 13:48:11 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        double purchasePrice = 350.50;
+        double salesTax = 0;
+        final double SALES_TAX_RATE = 0.05;
+
+        salesTax = purchasePrice * SALES_TAX_RATE;
+
+        System.out.println("the sales tax on a purchase of" + purchasePrice + " is " + salesTax + ".");
+
+
+
+    }
+}
